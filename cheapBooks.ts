e9aa5d3397979a2fb1d03be9ca410d57fd3d5afe
Ben Williams <bwilliams@extensis.com>
2020-10-04 13:31:08 -0700

    Flesh out test

--- cheapBooks.ts
+++
@@ -1,6 +0,0 @@
-describe('Visits book store and finds cheapest book', () => {
-    it('Visits book store and finds cheapest book', () => {
-        cy.visit('https://www.barnesandnoble.com/');
-        cy.get(`[id="searchBarBN"]`).type('Left Hand of Darkness');
-        cy.click(.btn);
-    })
