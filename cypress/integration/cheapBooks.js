8f4b09c24b9c6b9b2ae89086f8212cf39267e56d
Ben Williams <bwilliams@extensis.com>
2020-10-24 15:16:43 -0700

    Add homeObj.js functions to test

--- cypress/integration/cheapBooks.js
+++ cypress/integration/cheapBooks.js
@@ -1,11 +1,16 @@
+import { visit, fillSearch, searchSubmit } from '../../page/homeObj';
+import { visitEachChild } from 'typescript';
+
 describe('Visits book store and finds cheapest book', () => {
     it('Visits book store and finds cheapest book', () => {
-        cy.search('Ursula Le Guin');
+        visit();
+        fillSearch('Ursula Le Guin');
+        searchSubmit();
         cy.get(`[id="sortProducts1-replacement"]`).click();
         cy.get(`[id="sortProducts1-option-4"]`).click();
     });
 
-    it('Visits book store and adds 3 books to cart', () => {
+    it.skip('Visits book store and adds 3 books to cart', () => {
         cy.search('Philip Pullman');
         cy.get(`[id="sortProducts1-replacement"]`).click();
         cy.get(`[id="sortProducts1-option-0"]`).click();
